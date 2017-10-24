9818723bb9de500886272edc8ac530d8fd7bd307
Багин Денис <b.denio34@gmail.com>
2017-10-25 00:49:12 +0300

    Header fle

--- RaceEmulation/Includes.h
+++ RaceEmulation/Includes.h
@@ -2,13 +2,21 @@
 #define __INCLUDES_H__
 
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 #include <cstdlib>
+#include <conio.h>
+#include <Windows.h>
+#include <vector>
+#include <clocale>
+#include <thread>
+using namespace std;
 
 #include "RaceField.h"
 #include "ComplicatedField.h"
 #include "EasyField.h"
-#include "GameHandler.h"
-#include "Player.h"
+#include "ConsoleHandler.h"
+#include "GameProcessor.h"
+#include "Timer.h"
 
 #endif
