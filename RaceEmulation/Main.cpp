b41695386101dd4859b1e9b8f4e00d2ce77ece70
Багин Денис <b.denio34@gmail.com>
2017-10-25 00:44:57 +0300

    Can move and crush.

--- RaceEmulation/Main.cpp
+++ RaceEmulation/Main.cpp
@@ -3,4 +3,43 @@
 void main()
 {
 	srand(static_cast<unsigned int>(0));
+
+	RaceField * field = new EasyField();
+	GameProcessor gameProcessor(field);
+
+	HANDLE handleStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	Timer timer;
+	timer.start();
+	
+	ConsoleHandler::setCursorState(false);
+
+	gameProcessor.placePlayerCar();
+
+	while (true)
+	{
+		while (!_kbhit()) 
+		{
+			timer.asyncTime();
+			gameProcessor.makeGameTick();
+
+			ConsoleHandler::setCursorPosition(handleStdOut, 45, 5);
+			timer.showTime();
+			ConsoleHandler::setCursorPosition(handleStdOut, 45, 6);
+			gameProcessor.showTraveledDistance();
+
+			ConsoleHandler::setCursorPosition(handleStdOut, 1, 1);
+			field->draw();
+		}
+
+		std::flush(cout);
+		int key = _getch();
+
+		bool wasArrowPressed = (key == gameProcessor.DIRECTION_LEFT);
+			wasArrowPressed |= (key == gameProcessor.DIRECTION_RIGHT);
+
+		if (wasArrowPressed)
+			gameProcessor.moveCar(key);
+	}
+
 }
