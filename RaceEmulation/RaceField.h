d9dcbabf8d29a3fe0b777934f2a26bc5a19435a4
Багин Денис <b.denio34@gmail.com>
2017-10-25 00:48:48 +0300

    Base class to possible modes of Fields.

--- RaceEmulation/RaceField.h
+++ RaceEmulation/RaceField.h
@@ -4,20 +4,25 @@
 class RaceField
 {
 public:
-	virtual int				getHeight()															const = 0;
-	virtual int				getWidth()															const = 0;
-	virtual void			generateObstacle()														  = 0;
-	virtual void			placePlayerCar()														  = 0;
-	virtual void			drawField()															const = 0;
-protected:				
-	enum BlockType 
+	virtual int			getHeight()														const = 0;
+	virtual int			getWidth()														const = 0;
+	virtual void		draw()															const = 0;
+	virtual void		generateObstacle()													  = 0;
+	virtual void		setBlockType(const int& x, const int& y, const int& blockType)		  = 0;
+	virtual int			getBlockType(const int& x, const int& y)						const = 0;
+	virtual int			getBufferBlockType(const int& x)								const = 0;
+	virtual void		clearBuffer()														  = 0;
+	virtual void		placeObstacle()														  = 0;
+
+	enum BlockType
 	{
-		EMPTY		= 32,
-		BORDER		= 177,
-		OBSTACLE	= 205,
-		CAR_TOP		= 254,
-		CAR_TIRE	= 111,
-		CAR_TORSO	= 219
+		EMPTY = 32,
+		BORDER_ONE = 177,
+		BORDER_TWO = 176,
+		OBSTACLE = 205,
+		CAR_TOP = 254,
+		CAR_TIRE = 111,
+		CAR_TORSO = 219
 	};
 
 	enum Obstacle
