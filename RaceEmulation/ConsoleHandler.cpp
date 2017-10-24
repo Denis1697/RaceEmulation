7d38e5db766665b3084cba906314a79c6a218d0d
Багин Денис <b.denio34@gmail.com>
2017-10-25 00:45:24 +0300

    Work with console.

--- RaceEmulation/ConsoleHandler.cpp
+++ RaceEmulation/ConsoleHandler.cpp
@@ -6,3 +6,9 @@ ConsoleHandler::setCursorPosition(const HANDLE& handleStdOut, const int& x, cons
 	COORD coord = { x, y };
 	SetConsoleCursorPosition(handleStdOut, coord);
 }
+
+void ConsoleHandler::setCursorState(bool active)
+{
+	CONSOLE_CURSOR_INFO cci = { sizeof(cci), active };
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cci);
+}
--- RaceEmulation/ConsoleHandler.h
+++ RaceEmulation/ConsoleHandler.h
@@ -5,6 +5,7 @@ class ConsoleHandler
 {
 public:
 	static void setCursorPosition(const HANDLE& handleStdOut, const int& x, const int& y);
+	static void setCursorState(bool active);
 };
 
 #endif
